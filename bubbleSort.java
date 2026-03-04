db3b95770521ba686ad4381c78984119a8c728f3
Jyoti Kumari <jk1076387@gmail.com>
2026-03-04 19:25:16 +0530

    Add files via upload

---
+++ bubbleSort.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class bubbleSort {
+        public static void main(String  args[]){
+            Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();
+            int arr[]=new int[n];
+            for(int i=0;i<n;i++){
+                arr[i]=sc.nextInt();
+            }
+            bubbleS(arr);
+            System.out.println(Arrays.toString(arr));
+        }
+        static void bubbleS(int arr[]){
+            for(int i=0;i<arr.length;i++){
+                for(int j=1;j<arr.length-i;j++){
+                    if(arr[j]<arr[j-1]){
+                        int temp=arr[j];
+                        arr[j]=arr[j-1];
+                        arr[j-1]=temp;
+                    }
+                }
+            }
+        }
+}
