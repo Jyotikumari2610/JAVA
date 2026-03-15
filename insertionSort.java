d89f4697eafe3af6fde5888034c04c856ef51438
Jyoti Kumari <jk1076387@gmail.com>
2026-03-15 16:55:16 +0530

    Add files via upload

---
+++ insertionSort.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class insertionSort{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(insertion_Sort(arr));
+    }
+    static void insertion_Sort(int arr[]){
+        for(int i=1;i<arr.length;i++){
+            for(int j=i;j>0;j--){
+                if(arr[j]<arr[j-1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                }
+                else{
+                    break;
+                }
+            }
+        }
+    }
+}
